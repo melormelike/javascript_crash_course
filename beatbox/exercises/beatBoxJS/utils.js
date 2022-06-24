170ad50dab0de221816e383e49c85a597c4af251
Melike Gundogdu <gundogdu.mel@gmail.com>
2022-06-24 15:08:13 +0300

    beatbox exercise

---
+++ beatbox/exercises/beatBoxJS/utils.js
@@ -0,0 +1,65 @@
+/**
+ * Beat class that keeps track of playing the audio
+ * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
+ * HINT: Create a play function to play the audio if called
+ */
+class Beat {
+  constructor(audioSrc){
+    this.audio = new Audio(audioSrc);
+    //console.log(this.audio);
+
+  }
+    play = () => {
+      this.audio.currentTime = 0; /*when double pressed it is gonna reset and start the audio*/
+      this.audio.play()
+    }
+}
+
+
+
+
+/**
+ * Button class that keeps track of the button color based on a press
+ */
+class Button {
+    constructor(color, keyCode){
+      this.color = color;
+      this.keyCode = keyCode;
+      this.element = document.getElementById(keyCode);
+      this.setButtonColorInHTML();
+      this.setATransitionEndListener();
+      //console.log(this.element)
+
+    }
+    setATransitionEndListener = () => {
+      this.element.addEventListener('transitionend',() => {
+         //console.log("transitioned")
+        this.deselect()
+      })
+
+    }
+
+    /**
+     * Set the button color based on color specified
+     */
+    setButtonColorInHTML = () => {
+      this.element.style.borderColor = this.color;
+    }
+
+
+    /**
+     * Select function to set the background color and boxShadow
+     */
+    select = () => {
+      this.element.style.backgroundColor = this.color;
+      this.element.style.boxShadow = '0px 0px 17px 0px ${this.color}'
+    }
+
+    /**
+     * Deselect function to reset background color and boxShadow
+     */
+    deselect = () => {
+      this.element.style.backgroundColor = "transparent";
+      this.element.style.boxShadow = "none";
+    }
+}
